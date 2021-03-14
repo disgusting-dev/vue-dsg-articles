2a8c5c1a12d12cd473d4a6b8c02314ac64f68ead
aprok2 <anton.prokhorov@nike.com>
2021-03-14 14:23:43 +0100

    Directive - click outside

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p v-click-outside="{ handler: handleClickOutside }">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener" >vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
@@ -35,6 +35,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+
+  methods: {
+    handleClickOutside() {
+      console.log('I am outside');
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import clickOutside from './directives/clickOutside';
+
 Vue.config.productionTip = false
+
+Vue.directive('click-outside', clickOutside);
 
 new Vue({
   render: h => h(App),
